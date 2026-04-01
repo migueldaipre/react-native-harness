4c9364863cff155a640c195916ff32677f513555
Szymon Chmal <szymon@chmal.it>
2026-04-01 09:51:52 +0200

    refactor: remove unused reporters (#89)

--- packages/reporters/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { defaultReporter } from './default-reporter.js';
-export { junitReporter } from './junit-reporter.js';
-export type { Reporter } from './reporter.js';
--- packages/reporters/src/reporter.ts
+++
@@ -1,5 +0,0 @@
-import { TestSuiteResult } from '@react-native-harness/bridge';
-
-export type Reporter = {
-  report: (results: TestSuiteResult[]) => Promise<void>;
-};
